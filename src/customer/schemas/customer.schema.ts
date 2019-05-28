537a554257dc742cdfae099fccb09cc44531ddba
Luiza <luitays@mail.ru>
2019-05-28 19:32:30 +0300

    changes in customers' information

--- src/customer/schemas/customer.schema.ts
+++ src/customer/schemas/customer.schema.ts
@@ -1,11 +1,13 @@
 import * as mongoose from 'mongoose';
 
 export const CustomerSchema = new mongoose.Schema({
-    first_name: String,
     last_name: String,
+    first_name: String,
+    middle_name: String,
     email: String,
     phone: String,
     address: String,
-    description: String,
+    additionalInfo: String,
+    status: String,
     created_at: { type: Date, default: Date.now }
 })
