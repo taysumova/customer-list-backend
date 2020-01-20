cb8c19e2a64470404a1f4f5bd0076b1f4ad2d848
Luiza <luitays@mail.ru>
2020-01-20 16:23:19 +0300

    add survey module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './customer/customer.module';
+import { SurveyModule } from './survey/survey.module';
 
 @Module({
   imports: [
       MongooseModule.forRoot('mongodb+srv://admin:admin2019@cluster0-4wse7.mongodb.net/test?retryWrites=true' || 'mongodb://localhost/customer-app', { useNewUrlParser: true }),
-      CustomerModule
+      CustomerModule,
+      SurveyModule
   ],
   controllers: [AppController],
   providers: [AppService],
